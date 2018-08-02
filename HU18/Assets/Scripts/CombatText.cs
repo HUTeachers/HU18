e6a9fc18a36635f3e63b2cd0eb788a80b4cea99a
Møllnitz <cmolln10@student.aau.dk>
2018-08-02 21:43:03 +0200

    Sorta resolved text issue, still cannot reload scenes.

--- HU18/Assets/Scripts/CombatText.cs
+++ HU18/Assets/Scripts/CombatText.cs
@@ -1,27 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Runtime.InteropServices;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CombatText : MonoBehaviour {
+public class CombatText : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		GameManager.damageEvent += CombatTextDemonstration;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject textTemplate;
+    private void Start()
+    {
+        GameManager.damageEvent += CombatTextDemonstration;
+        GameManager.damageEvent += SpawnCombatText;
+    }
 
-	public void CombatTextDemonstration(GameObject damagetaker, float damage)
-	{
-		Debug.Log("Hej Daniel, vidste du at " + damagetaker.name + " tog hele " + damage + " skade");
-	}
+
+    public void CombatTextDemonstration(GameObject damagetaker, float damage)
+    {
+        Debug.Log("Hej Daniel, vidste du at " + damagetaker.name + " tog hele " + damage + " skade");
+    }
+
+    private void SpawnCombatText(GameObject damagetaker, float damage)
+    {
+        GameObject tt = Instantiate(textTemplate, transform);
+        tt.GetComponent<Text>().text = damage.ToString();
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(damagetaker.transform.position);
+        RectTransform rect = tt.GetComponent<RectTransform>();
+        //rect.anchorMin = viewPos;
+        //rect.anchorMax = viewPos;
+    }
 
     private void OnDisable()
     {
         GameManager.damageEvent -= CombatTextDemonstration;
+        GameManager.damageEvent -= SpawnCombatText;
     }
-
 }
