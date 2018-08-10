fd8524e6b1949142ee941725217fb50f404c2d08
Møllnitz <cmolln10@student.aau.dk>
2018-08-11 01:20:38 +0200

    Tilføjede PlaySoundOn Script og Editor

---
+++ HU18/Assets/Scripts/Editor/PlaySoundOnEditor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PlaySoundOn))]
+public class PlaySoundOnEditor : Editor {
+
+	bool showKeys;
+	PlaySoundOn scriptRef;
+	SoundEnum test;
+
+    void OnEnable()
+	{
+		scriptRef = target as PlaySoundOn;
+	}
+
+
+	public override void OnInspectorGUI()
+	{
+		//Authentic gray script field.	
+		GUI.enabled = false;
+		EditorGUILayout.ObjectField("Script", MonoScript.FromMonoBehaviour((PlaySoundOn)target), typeof(PlaySoundOn), false);
+		GUI.enabled = true;
+
+		scriptRef.soundEnum = (SoundEnum)EditorGUILayout.EnumPopup("Lyd på:", scriptRef.soundEnum);
+		EditorGUILayout.Space();
+
+		switch (scriptRef.soundEnum)
+		{
+
+			case SoundEnum.OnCollision:
+			case SoundEnum.OnTrigger:
+				scriptRef.RequireTag = EditorGUILayout.Toggle("Kræver tag?", scriptRef.RequireTag);
+				scriptRef.RequiredTag = EditorGUILayout.TextField(new GUIContent("Krævet tag:"), scriptRef.RequiredTag);
+				break;
+			case SoundEnum.OnActivate:
+				break;
+			case SoundEnum.OnInput:
+				List<KeyCode> temp = scriptRef.inputKeys;
+
+                int editorlistSize = Mathf.Max(0, EditorGUILayout.IntField("Antal keys", temp.Count));
+				
+				while(editorlistSize > temp.Count)
+				{
+					temp.Add(KeyCode.A);
+				}
+
+				while(editorlistSize < temp.Count )
+				{
+					temp.RemoveAt(temp.Count - 1);
+				}
+
+				EditorGUI.indentLevel++;
+				showKeys = EditorGUILayout.Foldout(showKeys,"Keys " );
+				if(showKeys)
+				{
+					for (int i = 0; i < temp.Count; i++)
+					{
+						temp[i] = (KeyCode)EditorGUILayout.EnumPopup(new GUIContent(i.ToString()), temp[i]);
+					}
+				}
+				
+				EditorGUI.indentLevel--;
+				break;
+			default:
+				break;
+		}
+	}
+
+
+}
---
+++ HU18/Assets/Scripts/PlaySoundOn.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Linq;
+
+public enum SoundEnum
+{
+    OnCollision,
+    OnTrigger,
+    OnActivate,
+    OnInput
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundOn : MonoBehaviour
+{
+
+    public SoundEnum soundEnum;
+
+    public List<KeyCode> inputKeys = new List<KeyCode>();
+
+    AudioSource aSource;
+
+    public bool RequireTag;
+    public string RequiredTag;
+
+    void Update()
+    {
+        if (soundEnum == SoundEnum.OnInput)
+        {
+            //inputKeys.Find(x => Input.GetKeyDown(x));
+            foreach (KeyCode item in inputKeys)
+            {
+                if (Input.GetKeyDown(item))
+                {
+                    Randomize(aSource);
+                    aSource.Play();
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (soundEnum == SoundEnum.OnCollision)
+        {
+            if (RequireTag == false || RequireTag == true && RequiredTag == collision.transform.tag)
+            {
+                Play();
+            }
+
+
+        }
+    }
+
+    private void Play()
+    {
+
+        aSource = GetComponent<AudioSource>();
+        GameObject g = new GameObject()
+        {
+            name = "Audio object"
+        };
+        g.AddComponent<AudioSource>();
+        AudioSource bSource = g.GetComponent<AudioSource>();
+
+        CopyAudio(aSource, bSource);
+
+        Randomize(bSource);
+        bSource.Play();
+        Destroy(g, 10);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (soundEnum == SoundEnum.OnTrigger)
+        {
+            if (RequireTag == false || RequireTag == true && RequiredTag == collision.transform.tag)
+            {
+                Play();
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        aSource = GetComponent<AudioSource>();
+
+        if (soundEnum == SoundEnum.OnActivate)
+        {
+            Randomize(aSource);
+            aSource.Play();
+        }
+    }
+
+    private void Randomize(AudioSource aSource)
+    {
+        aSource.pitch = Random.Range(0.95f * aSource.pitch, 1.05f * aSource.pitch);
+        aSource.volume = Random.Range(0.95f * aSource.volume, 1.05f * aSource.volume);
+    }
+
+    private void CopyAudio(AudioSource source, AudioSource dest)
+    {
+        AudioClip ac = source.clip;
+        float pitch = source.pitch;
+        float volu = source.volume;
+        bool playOnAwake = source.playOnAwake;
+
+        dest.clip = ac;
+        dest.pitch = source.pitch;
+        dest.volume = volu;
+        dest.playOnAwake = playOnAwake;
+
+    }
+}
