2b3e41631ed4c741a7e3d51e151937c20fac5902
Møllnitz <cmolln10@student.aau.dk>
2018-09-28 10:53:42 +0200

    Tileset

---
+++ HU18/Assets/ChangeSceneTwo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneTwo : MonoBehaviour {
+
+	public string SceneToLoad = "Hej";
+
+	public void LoadScene()
+	{
+		SceneManager.LoadScene(SceneToLoad);
+	}
+
+}
